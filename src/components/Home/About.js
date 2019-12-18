eb5b0adfafb6164782d5db347fd72d279764e937
Resta Adiputra <resta.adiputra@myboost.id>
2019-12-18 16:12:01 +0700

    finish up about page

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import Title from '../Title'
+import React from 'react';
+
+// Components
+import Title from '../Title';
+
+// Styles
+import styles from '../../css/about.module.css';
+
+// Assets
+import img from '../../images/defaultBcg.jpeg';
 
 function About() {
   return (
-    <div>
-      Hello from about
-      <Title title='about' subtitle='us' />
-    </div>
-  )
+    <section className={styles.about}>
+      <Title title="about" subtitle="us" />
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={img} alt='about section' />
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>explore the difference</h4>
+          <p>Ad eiusmod dolore aute velit excepteur sunt elit exercitation.Laborum mollit officia qui tempor.</p>
+          <p>Ad eiusmod dolore aute velit excepteur sunt elit exercitation.Laborum mollit officia qui tempor.</p>
+          <button type='button' className='btn-primary'>read more</button>
+        </article>
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
