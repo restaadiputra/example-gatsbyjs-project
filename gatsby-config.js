16586ea987e27619915c45d2154cef45138b8db5
Resta Adiputra <resta.adiputra@myboost.id>
2019-12-19 11:05:44 +0700

    add source-filename plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
+  siteMetadata: {
+    title: `BackRoads`,
+    description: `Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure.`,
+    author: `@john_doe`
+  },
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
   ],
 }
